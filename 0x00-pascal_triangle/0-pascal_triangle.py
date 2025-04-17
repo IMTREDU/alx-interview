fbcb472361c88178d10f0b175b401e476211ed4a
IMTREDU <92468621+IMTREDU@users.noreply.github.com>
2025-04-17 15:52:24 +0200

    Nvm

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,13 +9,13 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
     while len(triangle) != n:
         previous = triangle[-1]
         current = [1]
         for i in range(len(previous) - 1):
             current.append(previous[i] + previous[i + 1])
-            current.append(1)
-            triangle.append(current)
-        return triangle
+        current.append(1)
+        triangle.append(current)
+    return triangle
