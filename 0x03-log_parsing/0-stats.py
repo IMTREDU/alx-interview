3fb9aa60e742af404b91d4a9095f68b8a594c1cf
IMTREDU <92468621+IMTREDU@users.noreply.github.com>
2025-05-08 17:39:43 +0300

    Back to square one

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,43 +6,47 @@ Script that reads stdin line by line and computes metrics:
 """
 
 import sys
+import re
+import signal
 
+status_codes = {
+    "200": 0, "301": 0, "400": 0,
+    "401": 0, "403": 0, "404": 0,
+    "405": 0, "500": 0
+}
+total_size = 0
+line_count = 0
 
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+def print_stats():
+    """Prints the metrics"""
+    print(f"File size: {total_size}")
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+        line = line.strip()
+        parts = line.split()
+        if len(parts) >= 7:
 
-        stlist = line.split()
-        count += 1
+            status_code = parts[-2]
+            file_size = parts[-1]
+            if status_code in status_codes:
+                try:
+                    status_codes[status_code] += 1
+                except Exception:
+                    pass
+            try:
+                total_size += int(file_size)
+            except Exception:
+                pass
+            line_count += 1
 
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
-
+            if line_count % 10 == 0:
+                print_stats()
 except KeyboardInterrupt:
-    printsts(sts, size)
+    print_stats()
     raise
+else:
+    print_stats()
